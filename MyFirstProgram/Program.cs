666b8be21c682856cf17a5f4bcdf33869e5c5a79
Tegaaumu <117090031+Tegaaumu@users.noreply.github.com>
2025-07-24 05:25:18 +0100

    move new file from Working-On-String Branch(#1)

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,4 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello,\" World!");
 Console.BackgroundColor = ConsoleColor.Red;
 Console.Title = "Tega Project";
+
+string myString;
+string my_string;
+string MyString;
+
+myString = "Hello, World!";
+
+string coolString = "Hello,  World";
+
+coolString = "Goodbye, World";
+
+string firstName = "John";
+string lastName = "Ben\n";
+string fullName = firstName + " " + lastName;
+
+Console.WriteLine(fullName);
+
+myString = Console.ReadLine();
+
+Console.WriteLine(myString.Length);
+Console.WriteLine(myString[0]);
+
+char myChar = 'a';
