4ccdefe70443abbea9f135dd6324c1699aedbd61
Tegaaumu <tegaumurhurhu@gmail.com>
2025-08-02 11:41:51 +0100

    The previous code was not poped so i did not see the main changes

--- BasicValueTypes/obj/Debug/net8.0/BasicValueTypes.AssemblyInfo.cs
+++ BasicValueTypes/obj/Debug/net8.0/BasicValueTypes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicValueTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d7e69a7fe293567d981ed0486360f1ba8aa2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fdd0edc73e7fff64abae0bc50ecb53e45c6ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicValueTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicValueTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
